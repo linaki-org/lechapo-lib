99c092f58bc91e8f048119b54316011ff9989133
Linaki.0rg <linaki@linaki.org>
2026-08-11 22:19:43 +0200

    A bit of cleanup in this 5 years old lib...

---
+++ README.md
@@ -0,0 +1,4 @@
+# Le Chapo Lib
+## (The Hat Library)
+Le Chapo Lib provides a tiny layer of abstraction to the Python TKinter library.  
+It is designed for beginners to start coding in Python by interacting with a cute little E.T wearing a hat.
--- src/lechapolib/__init__.py
+++ src/lechapolib/__init__.py
@@ -8,16 +8,19 @@ import threading as THR
 stamps=[]
 imgs=[]
 virtuals=[]
+
 def _verify():
     """System function, verify if window exists"""
     try:
-        fenetre
+        root_tk
     except NameError:
-        raise HatError("Window not exists")
+        raise ChapoError("Window not exists")
+    
 def _distance(sp1, sp2):
     x1, y1=_xy(sp1)
     x2, y2=_xy(sp2)
     return round(sqrt((x2-x1)**2 + (y2-y1)**2))
+
 def _xy(sp):
     pos=c.coords(sp.id)
     if sp.nature=="img":
@@ -29,8 +32,10 @@ def _xy(sp):
     x=(pos[0] + pos[2])/2
     y=(pos[1] + pos[3])/2
     return x, y
+
 def _collide(sp1, sp2):
     return _distance(sp1, sp2) < ((sp1.brut.height()/2) + (sp2.brut.height()/2))
+
 class Sprite:
     """Create a sprite"""
     def __init__(self, id, brut, nature):
@@ -39,8 +44,8 @@ class Sprite:
         self.id=id
         self.brut=brut
         self.nature=nature
-        if self.id in (corps, oeil, pupille, bouche, cou, chapeau, mots):
-            raise HatError("'%s' id reserved to system"%self.id)
+        if self.id in (chapi_body, chapi_eye, chapi_pupil, chapi_mouth, chapi_neck, chapi_chapo, words_zone):
+            raise ChapoError("'%s' id reserved to system"%self.id)
     def move(self, x, y):
         """Move the sprite"""
         _verify()
@@ -51,7 +56,7 @@ class Sprite:
         if spstate in ("hidden", "normal"):
             pass
         else:
-            raise HatError("Invalid option '%s'"%spstate)
+            raise ChapoError("Invalid option '%s'"%spstate)
         c.itemconfig(self.id, state=spstate)
     def hide(self):
         """Hide the Sprite"""
@@ -74,6 +79,7 @@ class _Virtual:
         self.id=id
         self.brut=brut
         self.nature=nature
+
 class _Brut:
     """System class"""
     def __init__(self, height, width):
@@ -86,6 +92,7 @@ class _Brut:
     def width(self):
         """Width"""
         return self.Vwidth
+
 def img(file, x, y):
     global imgs
     imgs.append(TK.PhotoImage(file=file))
@@ -93,8 +100,10 @@ def img(file, x, y):
     obj=Sprite(id, imgs[-1], "img")
     update()
     return obj
-class HatError(Exception):
+
+class ChapoError(Exception):
     pass
+
 class Music(THR.Thread):
     """This class play background music."""
     def __init__(self, file):
@@ -107,75 +116,81 @@ class Music(THR.Thread):
     def run(self):
         """Thread function run"""
         music(self.file)
+
 def init(height=300, width=400):
     """Create le_chapo_lib window and draw the character"""
-    global fenetre
+    global root_tk
     try:
-        fenetre
+        root_tk
     except:
         pass
     else:
-        raise HatError("Use one window only")
+        raise ChapoError("Use one window only")
     
     global c
-    global corps
-    global oeil
-    global pupille
-    global bouche
-    global cou
-    global chapeau
-    global mots
-    fenetre=TK.Tk()  #On crée l'objet Tk()
-    fenetre.title("The hat") #On regle le titre
-    c=TK.Canvas(fenetre, height=height, width=width)#On crée le canevas
+    global chapi_body
+    global chapi_eye
+    global chapi_pupil
+    global chapi_mouth
+    global chapi_neck
+    global chapi_chapo
+    global words_zone
+    root_tk=TK.Tk()
+    root_tk.title("Chap0")
+    c=TK.Canvas(root_tk, height=height, width=width)
     c.pack()
-    #Dessin de l'extra terrestre
-    corps, oeil, pupille, bouche, cou, chapeau=stamp(False, True)
-    mots=c.create_text(200, 280, text="")#On crée la zone à messages
+
+    # Let's draw Chapi !
+    chapi_body, chapi_eye, chapi_pupil, chapi_mouth, chapi_neck, chapi_chapo=stamp(False, True)
+    words_zone=c.create_text(200, 280, text="")
 
 def character_1():
-    global corps
-    global oeil
-    global pupille
-    global bouche
-    global cou
-    global chapeau
+    global chapi_body
+    global chapi_eye
+    global chapi_pupil
+    global chapi_mouth
+    global chapi_neck
+    global chapi_chapo
+
 def set_hat_color(color):
     """Change color of the hat"""
     _verify()
-    c.itemconfig(chapeau, fill=color)#Changement de chapo !
+    c.itemconfig(chapi_chapo, fill=color)#Changement de chapo !
         
-
 
 def open_mouth():
     """Open mouth of character"""
     _verify()
-    c.itemconfig(bouche, fill="black")#HAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    c.itemconfig(chapi_mouth, fill="black")#HAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
 def close_mouth():
     """Close mouth of character"""
     _verify()
-    c.itemconfig(bouche, fill="green")#MMMMMMMMMMMMMMMMMMMMMMMMMMMH
+    c.itemconfig(chapi_mouth, fill="green")#MMMMMMMMMMMMMMMMMMMMMMMMMMMH
+
 def giveback_hat():
     """Show the hat"""
     _verify()
-    c.itemconfig(chapeau, state="normal")#J'ai mon chapo à moi !
+    c.itemconfig(chapi_chapo, state="normal")#J'ai mon chapo à moi !
+
 def take_hat():
     """Hide the hat"""
     _verify()
-    c.itemconfig(chapeau, state="hidden")#Rends moi mon chapo !
+    c.itemconfig(chapi_chapo, state="hidden")#Rends moi mon chapo !
     
 def message(message):
     """Show a message"""
     _verify()
-    c.itemconfig(mots, text=message)#Crache le morceau !
+    c.itemconfig(words_zone, text=message)#Crache le morceau !
     update()
 
 def button(text, command):
-    """Create a button, bind click to (command) and return an object of button """
+    """Create a button, bind click to [command] and return an object of this button """
     _verify()
-    btn=TK.Button(fenetre, text=text, command=command)
+    btn=TK.Button(root_tk, text=text, command=command)
     btn.pack()
     return btn
+
 def blink():
     """Blink the eye"""
     _verify()
@@ -185,19 +200,21 @@ def blink():
     open_eye()
     update()
     sleep(0.1)
+
 def move(x, y):
-    """Move the character of (x, y)"""
+    """Move the character by [x, y]"""
     _verify()
-    c.move(corps, x, y)
-    c.move(oeil, x, y)
-    c.move(pupille, x, y)
-    c.move(bouche, x, y)
-    c.move(cou, x, y)
-    c.move(chapeau, x, y)
+    c.move(chapi_body, x, y)
+    c.move(chapi_eye, x, y)
+    c.move(chapi_pupil, x, y)
+    c.move(chapi_mouth, x, y)
+    c.move(chapi_neck, x, y)
+    c.move(chapi_chapo, x, y)
+
 def move_message(x, y):
-    """Move the message zone of (x, y)"""
+    """Move the message zone by [x, y]"""
     _verify()
-    c.move(mots, x, y)
+    c.move(words_zone, x, y)
 
 def bind(key, command):
     """Bind (key) to (command)"""
@@ -205,7 +222,7 @@ def bind(key, command):
     try:
         c.bind_all(key, command)
     except:
-        raise HatError("Invalid event '%s'"%key)
+        raise ChapoError("Invalid event '%s'"%key)
 
     
 
@@ -217,42 +234,54 @@ def music(file):
 def set_fullscreen(value=True):
     """if (value)=True, put your window on fullscreen;Else, put your window in normal (not on fullscreen)"""
     _verify()
-    fenetre.attributes('-fullscreen', value)
+    root_tk.attributes('-fullscreen', value)
 
 def mouse_coords():
     """Return mouse coordinates"""
     _verify()
-    return fenetre.winfo_pointerxy()
+    return root_tk.winfo_pointerxy()
 
 def set_background(color):
     """Change colour of background"""
     _verify()
     try:
-        fenetre["bg"]=color
+        root_tk["bg"]=color
         c.configure(bg=color)
     except:
-        raise HatError("Invalid color '%s'"%color)
+        raise ChapoError("Invalid color '%s'"%color)
+
 def destroy():
     """Destroy your window"""
     _verify()
-    global fenetre
-    fenetre.destroy()
-    del fenetre
+    global root_tk
+    root_tk.destroy()
+    del root_tk
+
 def update():
     """Update window"""
     _verify()
-    fenetre.update()
+    root_tk.update()
+
 def reset():
     """Reset window"""
     kill_window()
     init()
+
 def demo():
-    """Demo of le_chapo_lib"""
+    """Lechapo-lib demo"""
     kill_window()
     init()
     set_background("light pink")
-    message("My name is E.T.")
-    for x in range(5):
+    message("Hello !")
+    deroule("My name is Chapi!", 0.1)
+    sleep(1)
+    message("Look what I can do !")
+    sleep(0.5)
+    for i in range(5):
+        blink()
+        sleep(0.2)
+    message("")
+    for x in range(3):
         update()
         sleep(1)
         set_hat_color("red")
@@ -263,32 +292,35 @@ def demo():
         move(5, 0)
         update()
         sleep(0.1)
+
 def kill_window():
     """Kill window if it was destroyed by "X" button"""
-    global fenetre
+    global root_tk
     try:
-        fenetre.destroy()
+        root_tk.destroy()
     except:
-        raise HatError("Window is destroyed")
-    del fenetre
+        raise ChapoError("Window is destroyed")
+    del root_tk
+
 def stamp(addtostamps=True, makeVirtuals=False):
     """Stamp character and return the stamp.If addtostamps=True, the stamp is add to "le_chapo_lib.stamps\""""
     _verify()
     global stamps
-    corps=c.create_oval(100, 150, 300, 250, fill="green") #Corps
-    oeil=c.create_oval(170, 70, 230, 130, fill="white")#Oeil
-    pupille=c.create_oval(190, 90, 210, 110, fill="black")#Pupille
-    bouche=c.create_oval(150, 220, 250, 240, fill="red")#Bouche
-    cou=c.create_line(200, 150, 200, 130) #Cou
-    chapeau=c.create_polygon(180, 75, 220, 75, 200, 20, fill="blue")#Chapeau
-    thisstamp=corps, oeil, pupille, bouche, cou, chapeau
+    chapi_body=c.create_oval(100, 150, 300, 250, fill="green") #chapi_body
+    chapi_eye=c.create_oval(170, 70, 230, 130, fill="white")#chapi_eye
+    chapi_pupil=c.create_oval(190, 90, 210, 110, fill="black")#chapi_pupil
+    chapi_mouth=c.create_oval(150, 220, 250, 240, fill="red")#chapi_mouth
+    chapi_neck=c.create_line(200, 150, 200, 130) #chapi_neck
+    chapi_chapo=c.create_polygon(180, 75, 220, 75, 200, 20, fill="blue")#chapi_chapo
+    thisstamp=chapi_body, chapi_eye, chapi_pupil, chapi_mouth, chapi_neck, chapi_chapo
     if makeVirtuals:
-        tomake_virtuals=[(corps, 100, 200), (oeil, 60, 60), (chapeau, 55, 40)]
+        tomake_virtuals=[(chapi_body, 100, 200), (chapi_eye, 60, 60), (chapi_chapo, 55, 40)]
         virtuals=make_virtuals(tomake_virtuals)
     #Fin du dessin
     if addtostamps:
         stamps.append(thisstamp)
     return thisstamp
+
 def clearstamps():
     """Clear all stamps"""
     global stamps
@@ -297,48 +329,58 @@ def clearstamps():
             c.delete(y)
     update()
     stamps=[]
+
 def _change_state(spstate):
     """System function. Change the "state" attribute of the character"""
     _verify()
-    c.itemconfig(corps , state=spstate)
-    c.itemconfig(cou , state=spstate)
-    c.itemconfig(chapeau , state=spstate)
-    c.itemconfig(oeil , state=spstate)
-    c.itemconfig(pupille , state=spstate)
-    c.itemconfig(bouche , state=spstate)
+    c.itemconfig(chapi_body , state=spstate)
+    c.itemconfig(chapi_neck , state=spstate)
+    c.itemconfig(chapi_chapo , state=spstate)
+    c.itemconfig(chapi_eye , state=spstate)
+    c.itemconfig(chapi_pupil , state=spstate)
+    c.itemconfig(chapi_mouth , state=spstate)
+
 def hide():
     """Hide the character"""
     _verify()
     _change_state("hidden")
+
 def show():
     """Show the character"""
     _verify()
     _change_state("normal")
+
 def set_title(title):
     """Set window title"""
     _verify()
-    fenetre.title(title)
+    root_tk.title(title)
+
 def popup_yesno(a, b):
     """Show a popup window (yes or no)"""
     _verify()
     return TKBOX.askyesno(a, b)
+
 def popup(a, b):
     """Show a popup window (info)"""
     _verify()
     TKBOX.showinfo(a, b)
+
 def popup_okcancel(a, b):
     """Show a popup window (OK or Cancel)"""
     _verify()
     return TKBOX.askokcancel(a, b)
+
 def popup_yesynocancel(a, b):
     """Show a popup window (OK or Cancel)"""
     _verify()
     return TKBOX.askyesnocancel(a, b)
+
 def nothing():
-    """This function do...nothing !!! It's very pratical !!"""
+    """This function do...nothing !!! It's very useful in default function assignements !!"""
     pass
+
 def deroule(text, timeS, call1=nothing, call2=nothing):
-    """Show text progressivement"""
+    """Show text progressively"""
     _verify()
     m1=""
     tocall=True
@@ -352,21 +394,25 @@ def deroule(text, timeS, call1=nothing, call2=nothing):
         update()
         sleep(timeS)
         tocall=not tocall
+
 def close_eye():
-    """Close the eye"""
+    """Close Chapi's eye"""
     _verify()
-    c.itemconfig(oeil, fill="green")
-    c.itemconfig(pupille, state=TK.HIDDEN)
+    c.itemconfig(chapi_eye, fill="green")
+    c.itemconfig(chapi_pupil, state=TK.HIDDEN)
+
 def open_eye():
-    """open the eye"""
+    """Open Chapi's eye"""
     _verify()
-    c.itemconfig(oeil, fill="white")
-    c.itemconfig(pupille, state=TK.NORMAL)
+    c.itemconfig(chapi_eye, fill="white")
+    c.itemconfig(chapi_pupil, state=TK.NORMAL)
+
 def is_on_collide(sp, sprites=virtuals):
     for sprite in sprites:
         if _collide(sprite, sp):
             return True
     return False
+
 def make_virtuals(virtuals):
     sprites=[]
     for id, height, width in virtuals:
@@ -374,3 +420,8 @@ def make_virtuals(virtuals):
         sprite=_Virtual(id, brut, "polygon")
         sprites.append(sprite)
     return sprites
+
+def mainloop():
+    """Run the game's mainloop"""
+    _verify()
+    root_tk.mainloop()
